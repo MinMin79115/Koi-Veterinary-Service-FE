a70e30a2e764406cbf4d07a1f93df80502ab897f
vnthuan01 <156802979+vnthuan01@users.noreply.github.com>
2024-10-22 19:13:03 +0700

    Update Manager.jsx

--- vite-project/src/pages/admin/Manager.jsx
+++ vite-project/src/pages/admin/Manager.jsx
@@ -4,20 +4,13 @@ import {
   PieChartOutlined,
   BarChartOutlined,
   UserOutlined,
-<<<<<<< HEAD
-=======
   CustomerServiceOutlined,
   CalendarOutlined 
->>>>>>> be0869eaf5d981e5045dbd09818a5d79b2d28ac0
 } from '@ant-design/icons';
 import { Breadcrumb, Layout, Menu, theme } from 'antd';
 import StaffManagement from './StaffManagement';
 import Dashboard from './Dashboard';
 import AdminProfile from './AdminPage';
-<<<<<<< HEAD
-const { Header, Content, Sider } = Layout;
-import { useNavigate } from 'react-router-dom';
-=======
 import FAQManagement from './FAQ';
 import ServiceManagement from './ServiceManagement';
 import SlotManagement from './SlotManagement';
@@ -26,7 +19,6 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../redux/features/userSlider';
 
->>>>>>> be0869eaf5d981e5045dbd09818a5d79b2d28ac0
 function getItem(label, key, icon, children) {
   return {
     key,
@@ -39,11 +31,7 @@ function getItem(label, key, icon, children) {
 
 const Manager = () => {
   const navigate = useNavigate();
-<<<<<<< HEAD
-
-=======
   const dispatch = useDispatch();
->>>>>>> be0869eaf5d981e5045dbd09818a5d79b2d28ac0
   const [collapsed, setCollapsed] = useState(false);
   const [selectedKey, setSelectedKey] = useState('dashboard');
   const {
@@ -54,11 +42,8 @@ const Manager = () => {
     getItem('Dasboard', 'dashboard', <BarChartOutlined />),
     getItem('Manage Staff', 'staff', <PieChartOutlined />),
     getItem('Manage FAQ', 'faq', <DesktopOutlined />),
-<<<<<<< HEAD
-=======
     getItem('Manage Service', 'service', <CustomerServiceOutlined />),
     getItem('Manage Slot', 'slot', <CalendarOutlined />),
->>>>>>> be0869eaf5d981e5045dbd09818a5d79b2d28ac0
     getItem('User', 'sub1', <UserOutlined />, [
       getItem('Profile', 'profile'),
       getItem('Logout', 'logout'),
@@ -77,12 +62,6 @@ const Manager = () => {
       case 'staff':
         return <StaffManagement />;
       case 'faq':
-<<<<<<< HEAD
-        return <div>FAQ Management Content</div>;
-      case 'profile':
-        return <AdminProfile />;
-      case 'logout':
-=======
         return <FAQManagement />;
       case 'service':
         return <ServiceManagement />;
@@ -93,7 +72,6 @@ const Manager = () => {
       case 'logout':
         dispatch(logout())
         sessionStorage.clear()
->>>>>>> be0869eaf5d981e5045dbd09818a5d79b2d28ac0
         return navigate('/login');
       default:
         return <div>Select an option</div>;
@@ -112,18 +90,10 @@ const Manager = () => {
         />
       </Sider>
       <Layout>
-<<<<<<< HEAD
-        <Header style={{ padding: 0, background: colorBgContainer }} />
-        <Content style={{ margin: '0 16px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Manager</Breadcrumb.Item>
-            <Breadcrumb.Item>{selectedKey === 'dashboard' ? 'Dashboard' : selectedKey === 'staff' ? 'Staff Management' : selectedKey === 'faq' ? 'FAQ Management' : 'User Profile'}</Breadcrumb.Item>
-=======
         <Content style={{ margin: '0 16px' }}>
           <Breadcrumb style={{ margin: '16px 0' }}>
             <Breadcrumb.Item>Manager</Breadcrumb.Item>
             <Breadcrumb.Item>{selectedKey === 'dashboard' ? 'Dashboard' : selectedKey === 'staff' ? 'Staff Management' : selectedKey === 'faq' ? 'FAQ Management' : selectedKey === 'service' ? 'Service Management' : selectedKey === 'slot' ? 'Slot Management' : 'User Profile'}</Breadcrumb.Item>
->>>>>>> be0869eaf5d981e5045dbd09818a5d79b2d28ac0
           </Breadcrumb>
           <div
             style={{
