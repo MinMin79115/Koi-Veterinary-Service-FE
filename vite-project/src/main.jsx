173db0ea652bee1f000766f92cc0a7c5e30d33d4
vnthuan01 <156802979+vnthuan01@users.noreply.github.com>
2024-10-22 19:16:11 +0700

    Update main.jsx

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -4,13 +4,6 @@ import App from './App.jsx'
 import 'react-toastify/ReactToastify.css'
 import './index.css'
 import { ToastContainer } from 'react-toastify'
-<<<<<<< HEAD
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ToastContainer/>
-    <App/>
-=======
 import { store, persistor } from './redux/store'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
@@ -23,6 +16,5 @@ createRoot(document.getElementById('root')).render(
         <App/>
       </PersistGate>
     </Provider>
->>>>>>> be0869eaf5d981e5045dbd09818a5d79b2d28ac0
   </StrictMode>,
 )
