9f6af1763465378c7ba44fa8d88ccfede35a34d6
vnthuan01 <156802979+vnthuan01@users.noreply.github.com>
2024-10-22 19:17:28 +0700

    Update axios.js

--- vite-project/src/config/axios.js
+++ vite-project/src/config/axios.js
@@ -1,14 +1,4 @@
 import axios from 'axios';
-<<<<<<< HEAD
-import secureLocalStorage from 'react-secure-storage';
-
-const api = axios.create({
-    baseURL: 'http://14.225.220.131:8080/api/',
-});
-
-api.interceptors.request.use(config => {
-    const token = secureLocalStorage.getItem('userToken');
-=======
 
 const api = axios.create({
     // baseURL: 'http://14.225.206.168:8080/api/',
@@ -17,7 +7,6 @@ const api = axios.create({
 
 api.interceptors.request.use(config => {
     const token = sessionStorage.getItem('token');
->>>>>>> be0869eaf5d981e5045dbd09818a5d79b2d28ac0
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
